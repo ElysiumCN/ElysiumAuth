d9670ce4378773bb9d15c71c9f3e91406dec2976
Cibocaz <xiangyuanhulian@outlook.com>
2025-10-02 17:48:23 +0800

    创建项目

---
+++ Elysium Auth Paper/src/main/java/com/skilfully/elysium/elysium_auth/ElysiumAuthPaper.java
@@ -0,0 +1,17 @@
+package com.skilfully.elysium.elysium_auth;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class ElysiumAuthPaper extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
---
+++ Elysium Auth Velocity/src/main/templates/com/skilfully/elysium/elysium_auth/BuildConstants.java
@@ -0,0 +1,7 @@
+package com.skilfully.elysium.elysiumauthvelocity;
+
+// The constants are replaced before compilation
+public class BuildConstants {
+
+    public static final String VERSION = "${version}";
+}
